cdd9201ad1ee0ec37a6f0819f37242270bb4b4bb
mac <mac@192.168.0.100>
2025-08-30 23:20:05 +0700

    upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://andung:WCfjEgAzV3ixhZq8@cluster0.0thqjm0.mongodb.net/',
-    ),
+    // MongooseModule.forRoot(
+    //   'mongodb+srv://andung:X5FXBQoqOgxT92PM@cluster0.0thqjm0.mongodb.net/',
+    // ),
+
+    ConfigModule.forRoot({
+      isGlobal: true, // quan trọng
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
